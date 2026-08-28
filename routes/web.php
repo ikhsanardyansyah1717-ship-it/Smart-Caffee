97c2805943f55262b1a794dc1aa07e4f99f449bf
Ikhsan Ardyansyah <ikhsanardyansyah17@gmail.com>
2026-08-28 17:14:40 +0700

    update fitur login semua role

--- routes/web.php
+++ routes/web.php
@@ -50,6 +50,15 @@ Route::middleware('guest')->group(function () {
 
 Route::middleware('auth')->group(function () {
 
+    Route::get('/dashboard', [AuthController::class, 'redirectByRole'])
+        ->name('role.dashboard');
+
+    Route::post('/logout', [AuthController::class, 'logout'])
+        ->name('logout');
+});
+
+Route::middleware('auth')->group(function () {
+
     Route::post('/logout', [AuthController::class, 'logout'])
         ->name('logout');
 
